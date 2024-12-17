47cc3d81b85f35f55efd7344e684675a202c5ad0
baalooos <baalooos@hotmail.com>
2024-12-17 16:10:54 +0100

    missing terraform fmt

--- variables.tf
+++ variables.tf
@@ -5,32 +5,32 @@ variable "region" {
 
 variable "db_name" {
   description = "Name of the RDS instance"
-  default = "mydb"
+  default     = "mydb"
 }
 
 variable "db_engine" {
   description = "RDS Engine"
-  default = "mysql"
+  default     = "mysql"
 }
 
 variable "db_version" {
   description = "Engine version"
-  default = "8.0"
+  default     = "8.0"
 }
 
 variable "db_instance" {
   description = "Instance type"
-  default = "db.t4g.micro"
+  default     = "db.t4g.micro"
 }
 
 variable "db_storage" {
   description = "Storage size"
-  default = "10"
+  default     = "10"
 }
 
 variable "db_username" {
   description = "Username"
-  default = "foo"
+  default     = "foo"
 }
 
 variable "db_password" {
@@ -41,10 +41,10 @@ variable "db_password" {
 
 variable "db_enable_bg" {
   description = "Enable Blue Green for this instance"
-  default = true
+  default     = true
 }
 
 variable "subnet_ids" {
   description = "Subnet Ids to create the Subnet Group"
-  default = ["subnet-00264e0b187ed44d0", "subnet-0147bc0620b7b4f7e", "subnet-02a4ebab3144711e1"]
+  default     = ["subnet-00264e0b187ed44d0", "subnet-0147bc0620b7b4f7e", "subnet-02a4ebab3144711e1"]
 }
